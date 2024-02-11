fb67ae04f2e249d448a7ffdc6e5ae87433a668eb
claraarnold <111783411+claraarnold@users.noreply.github.com>
2024-02-11 10:07:32 -0700

    Closes #53 (#60)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import Map from './Map';
+// import Chart from './Chart';
 import "./styles.css";
 
 function App() {
     return (
         <div>
             <h1>Hello, World!</h1>
+            <Map />
+            {/*<Chart />*/}
         </div>
     );
 }
