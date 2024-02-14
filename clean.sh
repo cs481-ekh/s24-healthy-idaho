a80e70d302699df4cc8f70672af34ae7cbcfd2c9
JoshuaC930 <joshuacorrales930@u.boisestate.edu>
2024-02-14 16:53:15 -0700

    testing stuffs

--- clean.sh
+++ clean.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
+echo "Cleaning up..."
+echo "Stopping container.."
 podman-compose down -v
+
+echo "Removing containers..."
+podman rmi -a -f
+
+echo "Killing processes on ports..."
 fuser -n tcp -k 3000
 fuser -n tcp -k 8000 
-exit 127
+exit 0
