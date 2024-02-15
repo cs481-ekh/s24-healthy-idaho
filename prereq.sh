00061dfd1cb80cd1f61b0fa59ed0999ac0230545
JoshuaC930 <joshuacorrales930@u.boisestate.edu>
2024-02-14 21:23:40 -0700

    updated prereq.sh

--- prereq.sh
+++ prereq.sh
@@ -2,6 +2,4 @@
 sudo apt-get update
 sudo apt install podman -y
 sudo apt install python3-pip -y
-pip3 install podman-compose
-sudo curl -o /usr/local/bin/podman-compose https://raw.githubusercontent.com/containers/podman-compose/devel/podman_compose.py
-sudo chmod +x /usr/local/bin/podman-compose
+sudo pip3 install https://github.com/containers/podman-compose/archive/devel.tar.gz
