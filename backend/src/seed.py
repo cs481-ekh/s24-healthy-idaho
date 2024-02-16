f8bf624518aedec86670760ba467a605e440e7a0
Anthony Diep <anthonydiep@u.boisestate.edu>
2024-02-16 12:57:20 -0700

    FUICKING WORKED

--- backend/src/seed.py
+++ backend/src/seed.py
@@ -1,8 +1,8 @@
 import mysql.connector 
-_user = "admin"
+_user = "root"
 _password = "pass"
 _host = "localhost"
 _database = "HEALTHY_IDAHO_DB"
 cnx = mysql.connector.connect(user=_user, password=_password, host=_host, database=_database)
-
+#
 cnx.close()
