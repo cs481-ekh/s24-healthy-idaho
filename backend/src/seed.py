d22bd778fb6dc1cb0befd7ba873208500c086d1e
Anthony Diep <anthonydiep@u.boisestate.edu>
2024-02-20 15:27:09 -0700

    est cnx to db & verified

--- backend/src/seed.py
+++ backend/src/seed.py
@@ -1,7 +1,12 @@
 import mysql.connector 
 import config
-cnx=mysql.connector.connect(user="admin", 
-                            password="password", 
+cnx=mysql.connector.connect(user="root", 
+                            password="rootpassword", 
                             host="localhost",
                             database="healthy_idaho_db") 
+cursor = cnx.cursor(buffered=True)
+
+
+
+cursor.close()
 cnx.close()
