08ee620d201eb9425943b82376b16571e3e42e24
Clara Arnold <claraarnold@u.boisestate.edu>
2024-02-27 11:00:50 -0700

    Filter bar adjusted

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,8 +6,13 @@ import "./styles.css";
 
 function App() {
     // Define options for COUNTY and FIPS dropdown menus
-    const countyOptions = ['CountyName1', 'CountyName2', 'CountyName3']; // Example options
-    const fipsOptions = ['16xxx', '16xxx', '16xxx']; // Example options
+    const yearOptions = ['2002', '2003', '2004', '2005', '2006', '2007', '2008', '2009',
+                                '2010', '2011', '2012', '2013', '2014', '2015', '2016', '2017',
+                                '2018', '2019', '2020'];
+    const variableOptions = ['Minority Status and Language', 'Overall Vulnerability',
+                                'Housing Type and Transportation', 'Household Composition and Disability',
+                                'Socioencomic Status', 'Greenness', 'Heat Island', 'LST-Max', 'LST-Mean',
+                                'Smoke', 'Population', 'PM-2-5'];
 
     return (
         <div>
@@ -15,7 +20,7 @@ function App() {
 
             <div className="app-container">
                 <div className="filter-bar-container">
-                    <FilterBar countyOptions={countyOptions} fipsOptions={fipsOptions} />
+                    <FilterBar yearOptions={yearOptions} variableOptions={variableOptions} />
                 </div>
 
                 <div className="map-container">
--- frontend/src/components/FilterBar.js
+++ frontend/src/components/FilterBar.js
@@ -1,50 +1,9 @@
 import React, { useState } from 'react';
 import "../styles.css";
 
-const FilterBar = ({ countyOptions, fipsOptions }) => {
-    const [selectedCounty, setSelectedCounty] = useState('');
-    const [selectedFips, setSelectedFips] = useState('');
-    const [selectAllAttributes, setSelectAllAttributes] = useState(false);
-    const [attributes, setAttributes] = useState({
-        minorityStatusAndLanguage: false,
-        overallVulnerability: false,
-        housingTypeAndTransportation: false,
-        householdCompositionAndDisability: false,
-        socioencomicStatus: false,
-        greenness: false,
-        heatIsland: false,
-        lstMax: false,
-        lstMean: false,
-        smoke: false,
-        population: false,
-        pm25: false,
-    });
-
-    const handleAttributeChange = (attribute) => {
-        setAttributes(prevState => ({
-            ...prevState,
-            [attribute]: !prevState[attribute]
-        }));
-    };
-
-    const handleSelectAll = () => {
-        if (selectAllAttributes) {
-            // deselect if all currently selected
-            const allAttributesFalse = Object.keys(attributes).reduce((acc, key) => {
-                acc[key] = false;
-                return acc;
-            }, {});
-            setAttributes(allAttributesFalse);
-        } else {
-            // select all attributes
-            const allAttributesTrue = Object.keys(attributes).reduce((acc, key) => {
-                acc[key] = true;
-                return acc;
-            }, {});
-            setAttributes(allAttributesTrue);
-        }
-        setSelectAllAttributes(!selectAllAttributes); // toggle
-    };
+const FilterBar = ({ yearOptions, variableOptions }) => {
+    const [selectedYear, setSelectedYear] = useState('');
+    const [selectedVariable, setSelectedVariable] = useState('');
 
     const handleSearch = () => {
         // Implement search functionality here
@@ -53,135 +12,30 @@ const FilterBar = ({ countyOptions, fipsOptions }) => {
 
     return (
         <div className="filter-bar">
-            {/* County Dropdown */}
+            {/* Year Dropdown */}
+            <label className="filter-label">Year</label>
             <div className="dropdown">
-                <select id="county" value={selectedCounty} onChange={(e) => setSelectedCounty(e.target.value)}>
-                    <option value="" disabled>Select County</option>
-                    {countyOptions.map((county, index) => (
-                        <option key={index} value={county}>{county}</option>
+                <select id="year" value={selectedYear} onChange={(e) => setSelectedYear(e.target.value)}>
+                    <option value="" disabled>Select Year</option>
+                    {yearOptions.map((year, index) => (
+                        <option key={index} value={year}>{year}</option>
                     ))}
                 </select>
             </div>
 
-            {/* FIPS Dropdown */}
+            {/* Variable Dropdown */}
+            <label className="filter-label">Variable</label>
             <div className="dropdown">
-                <select id="fips" value={selectedFips} onChange={(e) => setSelectedFips(e.target.value)}>
-                    <option value="" disabled>Select FIPS</option>
-                    {fipsOptions.map((fips, index) => (
-                        <option key={index} value={fips}>{fips}</option>
+                <select id="variable" value={selectedVariable} onChange={(e) => setSelectedVariable(e.target.value)}>
+                    <option value="" disabled>Select Variable</option>
+                    {variableOptions.map((variable, index) => (
+                        <option key={index} value={variable}>{variable}</option>
                     ))}
                 </select>
             </div>
 
-            {/* Filters */}
-            <div className="filter-box">
-                <label>
-                    <input
-                        type="checkbox"
-                        checked={selectAllAttributes}
-                        onChange={handleSelectAll}
-                    />
-                    Select All
-                </label>
-                <label>
-                    <input
-                        type="checkbox"
-                        checked={attributes.minorityStatusAndLanguage}
-                        onChange={() => handleAttributeChange('minorityStatusAndLanguage')}
-                    />
-                    Minority Status and Language:
-                </label>
-                <label>
-                    <input
-                        type="checkbox"
-                        checked={attributes.overallVulnerability}
-                        onChange={() => handleAttributeChange('overallVulnerability')}
-                    />
-                    Overall Vulnerability:
-                </label>
-                <label>
-                    <input
-                        type="checkbox"
-                        checked={attributes.housingTypeAndTransportation}
-                        onChange={() => handleAttributeChange('housingTypeAndTransportation')}
-                    />
-                    Housing Type and Transportation:
-                </label>
-                <label>
-                    <input
-                        type="checkbox"
-                        checked={attributes.householdCompositionAndDisability}
-                        onChange={() => handleAttributeChange('householdCompositionAndDisability')}
-                    />
-                    Household Composition and Disability:
-                </label>
-                <label>
-                    <input
-                        type="checkbox"
-                        checked={attributes.socioencomicStatus}
-                        onChange={() => handleAttributeChange('socioencomicStatus')}
-                    />
-                    Socioencomic Status:
-                </label>
-                <label>
-                    <input
-                        type="checkbox"
-                        checked={attributes.greenness}
-                        onChange={() => handleAttributeChange('greenness')}
-                    />
-                    Greenness:
-                </label>
-                <label>
-                    <input
-                        type="checkbox"
-                        checked={attributes.heatIsland}
-                        onChange={() => handleAttributeChange('heatIsland')}
-                    />
-                    Heat Island:
-                </label>
-                <label>
-                    <input
-                        type="checkbox"
-                        checked={attributes.lstMax}
-                        onChange={() => handleAttributeChange('lstMax')}
-                    />
-                    LST-Max:
-                </label>
-                <label>
-                    <input
-                        type="checkbox"
-                        checked={attributes.lstMean}
-                        onChange={() => handleAttributeChange('lstMean')}
-                    />
-                    LST-Mean:
-                </label>
-                <label>
-                    <input
-                        type="checkbox"
-                        checked={attributes.smoke}
-                        onChange={() => handleAttributeChange('smoke')}
-                    />
-                    Smoke:
-                </label>
-                <label>
-                    <input
-                        type="checkbox"
-                        checked={attributes.population}
-                        onChange={() => handleAttributeChange('population')}
-                    />
-                    Population:
-                </label>
-                <label>
-                    <input
-                        type="checkbox"
-                        checked={attributes.pm25}
-                        onChange={() => handleAttributeChange('pm25')}
-                    />
-                    PM-2-5:
-                </label>
-                {/* Search Button */}
-                <button onClick={handleSearch}>Search</button>
-            </div>
+            {/* Search Button */}
+            <button onClick={handleSearch}>Search</button>
         </div>
     );
 }
