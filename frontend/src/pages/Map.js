eda29fbc922fa29abbab047d760b9c64795510a5
JoshuaC930 <joshuacorrales930@u.boisestate.edu>
2024-03-14 11:22:02 -0600

    started fix on hover and popups

--- frontend/src/pages/Map.js
+++ frontend/src/pages/Map.js
@@ -29,6 +29,16 @@ function onEachFeature(feature, layer, colorData, variableName) {
     if (feature.properties && colorData) {
         let fipsObject = colorData.find((item) => item.id === parseInt(feature.properties.FIPS));
         
+        const handleMouseOver = (event) => {
+            const layer = event.target;
+            layer.setStyle({fillColor: 'yellow' });   
+        };
+        
+        const handleMouseOut = (event) => {
+            const layer = event.target;
+            const originalColor = fipsObject.color != null ? fipsObject.color : 'white';
+            layer.setStyle({ fillColor : originalColor });
+        };
 
         if(fipsObject && fipsObject.color != null) {
             layer.setStyle({fillColor: fipsObject.color, weight: 1, fillOpacity: 1});
@@ -41,6 +51,11 @@ function onEachFeature(feature, layer, colorData, variableName) {
                 (feature.properties.AREA_SQMI ? createPopupInfo("Area (sq. mi)", Math.round((feature.properties.AREA_SQMI + Number.EPSILON) * 100) / 100) : "") +
                 (feature.properties.LOCATION ? createPopupInfo("Location", feature.properties.LOCATION) : "")
             );
+
+            layer.on ({
+                mouseover: handleMouseOver,
+                mouseout: handleMouseOut
+            });
         }
     }
     else {
@@ -92,16 +107,7 @@ function Map({activeTract}) {
 
     }, [activeTract]);
     
-    const handleMouseOver = (event) => {
-        const layer = event.target;
-        layer.setStyle({fillColor: 'yellow' });   
-    };
-
-    const handleMouseOut = (event) => {
-        const layer = event.target;
-        const originalColor = layer.feature.properties.originalColor;
-        layer.setStyle({ fillColor : originalColor });
-    };
+    
     return (
         <MapContainer center={[45.394096, -114.734550]} zoom={6} style={{ height: '850px', width: '100%' }}>
             <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"/>
@@ -110,17 +116,8 @@ function Map({activeTract}) {
                     key={JSON.stringify(tractData) + JSON.stringify(colorData)}
                     style={{color: 'black', fillColor: 'black', weight: 1, fillOpacity: 0.25}}
                     data={tractData}
-                    onEachFeature={(feature, layer) => {
-                        const fipsObject = colorData.find(item => item.id === parseInt(feature.properties.FIPS));
-                        if (fipsObject && fipsObject.color != null) {
-                            layer.setStyle({ fillColor: fipsObject.color });
-                            layer.feature.properties.originalColor = fipsObject.color; // Store original color
-                        }
-                        layer.on({
-                            mouseover: handleMouseOver,
-                            mouseout: handleMouseOut,
-                        });
-                    }}
+                    onEachFeature={onEachFeature ? (feature, layer) => 
+                        onEachFeature(feature, layer, colorData, activeTract?.selectedVariable) : null}
                 />
             )}
         </MapContainer>
