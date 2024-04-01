9de62d637012204cabd9a9c5cb14c231c9ebbfe2
Clara Arnold <claraarnold@u.boisestate.edu>
2024-04-01 12:23:59 -0600

    Added admin login and redirect to AdminControl

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { useEffect } from 'react';
+import { useState } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import NavBar from './NavBar';
 import Compare from "./pages/Compare";
 import About from "./pages/About";
 import Data from "./pages/Data";
 import Admin from "./pages/Admin"
+import AdminControl from "./pages/AdminControl"
 import axios from 'axios';
 
 const App = () => {
@@ -18,6 +20,7 @@ const App = () => {
                     <Route path="/compare" element={<Compare />} />
                     <Route path="/about" element={<About />} />
                     <Route path="/admin" element={<Admin />} />
+                    <Route path="/admin-control" element={<AdminControl />} />
                     {/* Default route */}
                     <Route path="/" element={<Data />} />
                 </Routes>
---
+++ frontend/src/pages/AdminControl.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function AdminControl() {
+    return (
+        <div>
+            <h1>Administration Control</h1>
+            {/* Add your Administration Control content here */}
+        </div>
+    );
+}
+
+export default AdminControl;
