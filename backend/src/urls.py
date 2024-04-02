dd63fac286ba0e2a3e8e4599d800724fce01a734
Anthony Diep <atdiep66@gmail.com>
2024-04-02 09:25:53 -0600

    Closes #173 (#174)

--- backend/src/urls.py
+++ backend/src/urls.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from django.urls import path, include
 from .settings import APP_ROOT
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path(f'{APP_ROOT}/admin/', admin.site.urls),
     path(f'{APP_ROOT}/', include('healthy_idaho.urls'))
 ]
