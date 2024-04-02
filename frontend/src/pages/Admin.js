79695f95626e1baaf1178f730a19e103e0cdde79
claraarnold <111783411+claraarnold@users.noreply.github.com>
2024-04-02 09:22:57 -0600

    Closes #167 (#172)
    * Added admin login and redirect to AdminControl
    
    * Changed user/pass to environment variables

--- frontend/src/pages/Admin.js
+++ frontend/src/pages/Admin.js
@@ -1,13 +1,52 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import "../styles.css";
 
-function About() {
+function Admin() {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const [error, setError] = useState('');
+    const navigate = useNavigate();
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        // if (username === 'Healthy Idaho' && password === 'spring24') {
+        if (username === process.env.REACT_APP_USERNAME && password === process.env.REACT_APP_PASSWORD) {
+            // Successful login, you can redirect or do any further action here
+            // setError('');
+            // alert('Login successful!');
+            navigate('/admin-control');
+        } else {
+            setError('Invalid username or password');
+        }
+    };
 
     return (
-        <div>
-            <h1> Stretch goal: Admin page</h1>
+        <div className="login-container">
+            <div className={error ? 'error-message' : 'hidden'}>
+                {error && <p>{error}</p>}
+            </div>
+            <form onSubmit={handleSubmit}>
+                <div className="form-group">
+                    <label>Username</label>
+                    <input
+                        type="text"
+                        value={username}
+                        onChange={(e) => setUsername(e.target.value)}
+                    />
+                </div>
+                <div className="form-group">
+                    <label>Password</label>
+                    <input
+                        type="password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                    />
+                </div>
+                <button type="submit">Login</button>
+            </form>
         </div>
     );
 }
 
-export default About;
+export default Admin;
