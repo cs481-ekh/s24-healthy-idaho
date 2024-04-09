e19ba0e2dba93cdd866521d84c032266590a2ecc
JoshuaC930 <joshuacorrales930@u.boisestate.edu>
2024-04-08 18:07:33 -0600

    added manual mysql.cnf to force bind to localhost

--- sql/entrypoint.sh
+++ sql/entrypoint.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 
-/usr/local/bin/docker-entrypoint.sh mysqld >/dev/null 2>&1 &
-PID="$!"
+# Check if MySQL is already running
+if ! mysqladmin ping -h"localhost" --silent; then
+    /usr/local/bin/docker-entrypoint.sh mysqld >/dev/null 2>&1 &
+    PID="$!"
 
-while ! mysqladmin ping -h"localhost" --silent; do
-    echo "🟠 Wait for MySQL to be ready..."
-    sleep 1
-done
+    while ! mysqladmin ping -h"localhost" --silent; do
+        echo "🟠 Wait for MySQL to be ready..."
+        sleep 1
+    done
+else
+    echo "🟢 MySQL is already running."
+    PID=$(pgrep mysqld)
+fi
 
 echo "🟢 MySQL is ready."
 echo "🟦 Current user: $(whoami)"
@@ -16,10 +22,10 @@ python3 UploadData.py
 
 if [ $? -ne 0 ]; then
     echo "🔴 Data upload failed."
-    exit 1
-fi
 
-echo "🟢 Data uploaded."
-echo "🟢 Database service up and running!"
+else
+    echo "🟢 Data uploaded."
+    echo "🟢 Database service up and running!"
+fi
 
 wait "$PID"
